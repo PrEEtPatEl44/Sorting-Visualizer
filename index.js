b6ad55a811c0f20cc1676ba222be90f6210a148b
Preet Patel <pbpatel48@myseneca.ca>
2024-01-09 20:58:55 -0500

    added promises

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ function animate(swaps){
     console.log(swaps[0]);
     const [i,j]=swaps.shift();
     console.log(i+","+j);
-    const bars = document.querySelectorAll(".bar");
     // bars[i].style.height = arr[i] * 100 + "%";
     // bars[j].style.height = arr[j] * 100 + "%";
     let temp = arr[i];
@@ -49,29 +48,39 @@ function animate(swaps){
     },10);
 }
 function sort(){
-    const copy =[...arr];
-    console.log(copy);
-    const swaps=bubbleSort(copy);
-
-    animate(swaps);
+    // const copy =[...arr];
+    // console.log(copy);
+    // const swaps=bubbleSort(copy);
+    
+    // animate(swaps);
     // selectionSort(arr);
+    bubbleSort();
 }
-function bubbleSort(arr){
-    const swaps=[];
+async function bubbleSort(){
+    // const swaps=[];
     do{
         var swapped=false;
+        const bars = document.querySelectorAll(".bar");
         for(let i=0; i<arr.length-1; i++){
             if(arr[i]>arr[i+1]){
-                swaps.push([i,i+1]);
-                console.log(swaps);
+                // swaps.push([i,i+1]);
+                // console.log(swaps);
                 let temp = arr[i];
                 arr[i]=arr[i+1];
                 arr[i+1]=temp;
                 swapped=true;
+                bars[i].style.height = arr[i] * 100 + "%";
+                bars[i+1].style.height = arr[i+1] * 100 + "%";
+                bars[i].style.backgroundColor = "red";
+                bars[i+1].style.backgroundColor= "red";
+                await new Promise((resolve) => setTimeout(resolve, 10));;
             }
+            bars[i].style.backgroundColor = "black";
+            bars[i+1].style.backgroundColor= "black";
         }
+        await new Promise((resolve) => setTimeout(resolve, 10));
     }while(swapped);
-    return swaps
+    // return swaps
     // show();
     // animate(swaps);
   
